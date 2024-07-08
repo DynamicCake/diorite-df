3cc09ccfe719dd98228e5e7e12f5bd127c576581
DynamicCake <128557765+DynamicCake@users.noreply.github.com>
2024-07-08 21:45:47 +0800

    Add tests Added tests for lexer, parser and a little for codegen.

---
+++ src/parser/test.rs
@@ -0,0 +1,117 @@
+//! Parser tests are conducted here
+//! No semantic analysis here so names do not need to be valid
+//! Sometimes `gaction JankCheck ()` is added to make sure the parser is in an okay state
+use super::ParsedFile;
+use crate::{
+    error::syntax::{LexerError, UnexpectedEOF, UnexpectedToken},
+    parser::Parser,
+};
+use lasso::ThreadedRodeo;
+use logos::Lexer;
+use std::sync::Arc;
+
+
+type ParseResult = Result<(), ParseFileErrors>;
+
+#[derive(Debug)]
+struct ParseFileErrors {
+    pub lex_errs: Vec<LexerError>,
+    pub parse_errs: Vec<UnexpectedToken>,
+    pub at_eof: Option<Box<UnexpectedEOF>>,
+}
+
+/// Transform source to [ParseResult]
+fn parse_string(src: &str) -> ParseResult {
+    let rodeo = Arc::new(ThreadedRodeo::new());
+    let lexer = Lexer::with_extras(src, rodeo);
+    let file = Parser::parse(lexer, "[test]".into());
+    if file.is_successful() {
+        Ok(())
+    } else {
+        Err(ParseFileErrors {
+            lex_errs: file.lex_errs,
+            parse_errs: file.parse_errs,
+            at_eof: file.at_eof,
+        })
+    }
+}
+
+
+#[test]
+fn sanity() -> ParseResult {
+    parse_string("")
+}
+
+#[test]
+fn single() -> ParseResult {
+    parse_string(
+        r#"
+        pevent Exist 
+            paction SendMessage ("Everything is fine")
+        end
+        "#,
+    )
+}
+
+#[test]
+fn top_level() -> ParseResult {
+    parse_string(
+        r#"
+        pevent Join
+        end
+
+        eevent Die
+        end
+
+        proc GameLoop
+        end
+
+        func the(name: type description, other: type here)
+        end
+    "#,
+    )
+}
+
+#[test]
+fn nest_repeat() -> ParseResult {
+    parse_string(
+        r#"
+        pevent Join
+            repeat While <IsSneaking> ()
+                repeat IDunno ['Tag 2': 'Electric Boogaloo'] ()
+                    paction SendMessage ("Tag! you're it!")
+                end
+            end
+        end
+    "#,
+    )
+}
+
+#[test]
+fn nest_if() -> ParseResult {
+    parse_string(
+        r#"
+        pevent Join
+            ifplayer not Sneaking <default> ()
+                ifgame HasLigma [Strict: 'Maybe So'] ()
+                    paction SendMessage ("Ligma nuts")
+                end
+            end
+            gaction JankCheck ()
+        end
+        "#
+    )
+}
+
+#[test]
+fn items() -> ParseResult {
+    parse_string(
+        r#"
+        func the()
+            paction GiveItem (item('{Count:1b,id:"minecraft:diamond_sword"}'))
+            gaction JankCheck ()
+        end
+        "#
+    )
+}
+
--- src/test.rs
+++ src/test.rs
@@ -1,18 +1,90 @@
-//! Testing place here, I promise there will be more tests
+//! Tests for modules without mod.rs like [crate::lexer]
+
+// Lexer
+
+use std::sync::Arc;
+
+use lasso::{Reader, Spur, ThreadedRodeo};
+use logos::{Lexer, Logos};
+
+use crate::lexer::Token;
+
+// Asserts that the next token is a certain token
+macro_rules! next_eq {
+    ($lexer:expr, $tok:expr) => {
+        assert_eq!($lexer.next().unwrap().unwrap(), $tok);
+    };
+}
 
 #[test]
-fn print_tes() {
+fn lexer_test_basic() {
+    let src = r#"
+        paction eaction gaction
+        control callf callp
+        ifplayer ifentity ifgame ifvar
+        repeat select var
+        pevent eevent proc func
+        end not else
+        , : () {} [] <>
+    "#;
+
+    let rodeo = Arc::new(ThreadedRodeo::new());
+    let mut lex = Token::lexer_with_extras(src, rodeo);
+
+    next_eq!(lex, Token::PlayerAction);
+    next_eq!(lex, Token::EntityAction);
+    next_eq!(lex, Token::GameAction);
+
+    next_eq!(lex, Token::Control);
+    next_eq!(lex, Token::CallFunction);
+    next_eq!(lex, Token::CallProcess);
+
+    next_eq!(lex, Token::IfPlayer);
+    next_eq!(lex, Token::IfEntity);
+    next_eq!(lex, Token::IfGame);
+    next_eq!(lex, Token::IfVar);
+
+    next_eq!(lex, Token::Repeat);
+    next_eq!(lex, Token::Select);
+    next_eq!(lex, Token::SetVar);
+
+    next_eq!(lex, Token::PlayerEvent);
+    next_eq!(lex, Token::EntityEvent);
+    next_eq!(lex, Token::ProcDef);
+    next_eq!(lex, Token::FuncDef);
+
+    next_eq!(lex, Token::End);
+    next_eq!(lex, Token::Not);
+    next_eq!(lex, Token::Else);
+
+    next_eq!(lex, Token::Comma);
+    next_eq!(lex, Token::Colon);
+
+    next_eq!(lex, Token::OpenParen);
+    next_eq!(lex, Token::CloseParen);
+    next_eq!(lex, Token::OpenBrace);
+    next_eq!(lex, Token::CloseBrace);
+    next_eq!(lex, Token::OpenBracket);
+    next_eq!(lex, Token::CloseBracket);
+    next_eq!(lex, Token::OpenComp);
+    next_eq!(lex, Token::CloseComp);
+}
+
+#[test]
+fn tok_eq() {
     use crate::lexer::Token;
     use lasso::ThreadedRodeo;
 
     let rodeo = ThreadedRodeo::new();
-    let tok = Token::Iden(Some(rodeo.get_or_intern("ur mom")));
-    let tok2 = Token::Iden(Some(rodeo.get_or_intern("ur dad")));
-    println!("{:?}", (tok == tok2));
+    let a1 = Token::Iden(Some(rodeo.get_or_intern("tok a")));
+    let a2 = Token::Iden(Some(rodeo.get_or_intern("tok a")));
+    let b1 = Token::Iden(Some(rodeo.get_or_intern("tok b")));
+    assert_eq!(a1, a2);
+    assert_eq!(a1, b1);
 }
 
 #[test]
-fn iden_quotes() {
+fn lexer_iden() {
     use lasso::ThreadedRodeo;
     use logos::Logos;
     use std::sync::Arc;
@@ -20,24 +92,55 @@ fn iden_quotes() {
     use crate::lexer::Token;
 
     let rodeo = Arc::new(ThreadedRodeo::new());
-    const SRC: &str = "'hello'";
-    let mut lexer = Token::lexer(SRC);
-    let left = lexer.next().unwrap().unwrap();
-    let right = Token::Iden(Some(rodeo.get_or_intern("hello")));
-    assert_eq!(left, right);
+    let src = "'hello'";
+    let lexer = Token::lexer_with_extras(src, rodeo.clone());
+    lexer.collect::<Vec<_>>(); // consume
+    assert!(rodeo.contains("hello"));
 }
 
 #[test]
-fn unexpected() {
-    use crate::error::syntax::{ExpectedTokens, UnexpectedToken};
-    use crate::lexer::Token;
+fn lexer_string() {
+    use lasso::ThreadedRodeo;
+    use logos::Logos;
     use std::sync::Arc;
 
-    UnexpectedToken::new(
-        ExpectedTokens::new(Arc::new([Token::Iden(None)])),
-        Token::Colon.spanned(1..3),
-        Some("les go".into()),
-        "test".into(),
-    )
-    .expected_print();
+    use crate::lexer::Token;
+
+    let rodeo = Arc::new(ThreadedRodeo::new());
+    let src = "\"world\"";
+    let lexer = Token::lexer_with_extras(src, rodeo.clone());
+    lexer.collect::<Vec<_>>(); // consume
+    assert!(rodeo.contains("world"));
 }
+
+#[test]
+fn lexer_multiline() {
+    // Yes, it should be formatted like that
+    let src = r#"
+        paction Join
+            paction SendMessage ("=====
+Welcome to my Plot!
+=====")
+        end
+    "#;
+    let rodeo = Arc::new(ThreadedRodeo::new());
+    let lexer = Token::lexer_with_extras(src, rodeo.clone());
+    lexer.collect::<Vec<_>>(); // consume
+    assert!(rodeo.contains(r#"=====
+Welcome to my Plot!
+====="#));
+}
+
+#[test]
+fn lexer_styled() {
+    let src = r#"
+        paction Join
+            paction SendMessage ($"<white>balls")
+        end
+    "#;
+    let rodeo = Arc::new(ThreadedRodeo::new());
+    let lexer = Token::lexer_with_extras(src, rodeo.clone());
+    lexer.collect::<Vec<_>>(); // consume
+    assert!(rodeo.contains("<white>balls")); // This is better than &fballs fight me
+}
+
